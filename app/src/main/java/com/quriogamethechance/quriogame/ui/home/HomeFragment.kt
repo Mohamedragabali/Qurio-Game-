8e5dd1c4ce2b2683ec26a46651000f968cd77307
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-10-01 02:24:43 +0300

    create home screen and create graph of app

---
+++ app/src/main/java/com/quriogamethechance/quriogame/ui/home/HomeFragment.kt
@@ -0,0 +1,16 @@
+package com.quriogamethechance.quriogame.ui.home
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.quriogamethechance.quriogame.databinding.FragmentHomeBinding
+import com.quriogamethechance.quriogame.ui.BaseFragment
+
+class HomeFragment : BaseFragment<FragmentHomeBinding>() {
+    override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentHomeBinding
+        get() =  FragmentHomeBinding::inflate
+
+    override fun setup() {
+
+    }
+
+}
