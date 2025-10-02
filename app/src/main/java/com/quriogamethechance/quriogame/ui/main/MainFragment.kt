e3c67080ae156b3265d06dcc8d7ab8d0885fd3f4
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-10-03 00:55:17 +0300

    initial setup in main fragment

--- app/src/main/java/com/quriogamethechance/quriogame/ui/main/MainFragment.kt
+++ app/src/main/java/com/quriogamethechance/quriogame/ui/main/MainFragment.kt
@@ -2,6 +2,7 @@ package com.quriogamethechance.quriogame.ui.main
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.quriogamethechance.quriogame.databinding.FragmentMainBinding
 import com.quriogamethechance.quriogame.ui.BaseFragment
 
@@ -11,7 +12,8 @@ class MainFragment : BaseFragment<FragmentMainBinding>() {
         get() =  FragmentMainBinding::inflate
 
     override fun setup() {
-
+        val action = MainFragmentDirections.actionMainFragmentToFirstOnboardingFragment()
+        binding.root.findNavController().navigate(action)
     }
 
 }
