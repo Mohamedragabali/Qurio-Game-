1a34dec86490fb4464211cbe833ced7dc64d300b
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-10-03 20:01:10 +0300

    finish onboarding fragment design

--- app/src/main/java/com/quriogamethechance/quriogame/ui/onboarding/FourthOnboardingFragment.kt
+++ app/src/main/java/com/quriogamethechance/quriogame/ui/onboarding/FourthOnboardingFragment.kt
@@ -1,16 +1,120 @@
 package com.quriogamethechance.quriogame.ui.onboarding
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.navigation.findNavController
+import com.quriogamethechance.quriogame.R
 import com.quriogamethechance.quriogame.databinding.FragmentFourthOnboardingBinding
 import com.quriogamethechance.quriogame.ui.BaseFragment
 
 class FourthOnboardingFragment : BaseFragment<FragmentFourthOnboardingBinding>() {
     override val bindingInflater: (LayoutInflater, ViewGroup?, Boolean) -> FragmentFourthOnboardingBinding
         get() = FragmentFourthOnboardingBinding::inflate
+    private val animatedArrowDuration = 1000L
+    private val startBottomArrowDelay = 400L
+    private val startMiddleArrowDelay = 750L
+    private val startTopArrowDelay = 950L
 
     override fun setup() {
-
+        initialButton()
+        initialSwipeButton()
+        initialTextColor()
+        initialArrowsAnimation()
     }
+
+    private fun initialButton() {
+        binding.backButton.setOnClickListener {
+            it.findNavController().popBackStack()
+        }
+        binding.nextButton.setOnClickListener {
+            val action = FourthOnboardingFragmentDirections.actionFourthOnboardingFragmentToHomeFragment()
+            it.findNavController().navigate(action)
+        }
+    }
+
+
+    private fun initialTextColor() {
+        val positions = floatArrayOf(
+            0.6f,
+            1f
+        )
+        val textShader = LinearGradient(
+            0f, 0f, 0f, binding.swipeToPlay.textSize,
+            intArrayOf(
+                ContextCompat.getColor(requireContext(), R.color.swipe_text_color),
+                ContextCompat.getColor(requireContext(), R.color.primary)
+            ),
+            positions,
+            Shader.TileMode.CLAMP
+        )
+
+        binding.swipeToPlay.paint.shader = textShader
+        binding.swipeToPlay.invalidate()
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun initialSwipeButton() {
+        binding.swipeContainer.post {
+            val maxDrag = binding.swipeContainer.height - binding.swipeHandle.height
+            var dY = 0f
+
+            binding.swipeHandle.setOnTouchListener { v, event ->
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        dY = v.y - event.rawY
+                        true
+                    }
+
+                    MotionEvent.ACTION_MOVE -> {
+                        var newY = event.rawY + dY
+                        newY = newY.coerceIn(0f, maxDrag.toFloat())
+                        v.y = newY
+                        true
+                    }
+
+                    MotionEvent.ACTION_UP -> {
+                        if (v.y <= 50f) {
+                            val action =
+                                FourthOnboardingFragmentDirections.actionFourthOnboardingFragmentToHomeFragment()
+                            v.findNavController().navigate(action)
+                        }
+                        v.animate().y(maxDrag.toFloat()).setDuration(200).start()
+                        true
+                    }
+
+                    else -> false
+                }
+            }
+        }
+    }
+
+
+    private fun initialArrowsAnimation() {
+        initialInfiniteArrowAnimation(binding.bottomArrow, startBottomArrowDelay)
+        initialInfiniteArrowAnimation(binding.middleArrow, startMiddleArrowDelay)
+        initialInfiniteArrowAnimation(binding.topArrow, startTopArrowDelay)
+    }
+
+    private fun initialInfiniteArrowAnimation(
+        view: View,
+        startDelayValue: Long
+    ) {
+        val fadeInOut = ObjectAnimator.ofFloat(view, "alpha", 0f, 1f, 0f).apply {
+            duration = animatedArrowDuration
+            startDelay = startDelayValue
+            repeatCount = ValueAnimator.INFINITE
+            repeatMode = ValueAnimator.RESTART
+        }
+        fadeInOut.start()
+    }
+
 
 }
