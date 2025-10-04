58ebcf76dd99e90b26710955a81377389d85721a
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-10-04 17:09:48 +0300

    create BaseDialogFragment and initial themes and create bottom_sheet_color.xml

---
+++ app/src/main/java/com/quriogamethechance/quriogame/ui/BaseDialogFragment.kt
@@ -0,0 +1,36 @@
+package com.quriogamethechance.quriogame.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import androidx.viewbinding.ViewBinding
+import com.quriogamethechance.quriogame.R
+
+
+abstract class BaseDialogFragment<VB: ViewBinding>() : DialogFragment() {
+
+    override fun getTheme(): Int = R.style.CenteredDialogTheme
+    abstract val bindingInflater :(LayoutInflater, ViewGroup?, Boolean) -> VB
+    private var _binding : ViewBinding? = null
+    protected val binding : VB
+        get() = _binding as VB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bindingInflater(inflater,container,false)
+        return requireNotNull(_binding).root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setup()
+    }
+
+    abstract fun setup()
+
+}
