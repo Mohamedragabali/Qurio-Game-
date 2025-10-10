9b3def611938af06a3c0ab290bccf2192af20080
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-10-10 23:14:17 +0300

    build update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.navigation.safe.args) apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.7.7" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.9.5" apply false
 }
 
 buildscript {
