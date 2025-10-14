ff629d7b8fd09f2120e78cf4778ddda0273cdf62
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-10-15 01:23:59 +0300

    delete not important data class

--- app/src/main/java/com/quriogamethechance/quriogame/ui/characterDetails/CharacterDetails.kt
+++
@@ -1,8 +0,0 @@
-package com.quriogamethechance.quriogame.ui.characterDetails
-
-data class CharacterDetails(
-    val name : String ,
-    val age : String ,
-    val description : String ,
-    val imageResourceId : Int
-)
