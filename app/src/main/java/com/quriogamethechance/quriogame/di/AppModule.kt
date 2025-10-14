eea86ffba9a6dc87636c3cfe6fa663e25769ce83
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-10-14 12:48:56 +0300

    initial preferencesDataStore

--- app/src/main/java/com/quriogamethechance/quriogame/di/AppModule.kt
+++ app/src/main/java/com/quriogamethechance/quriogame/di/AppModule.kt
@@ -2,14 +2,22 @@ package com.quriogamethechance.quriogame.di
 
 import android.app.Application
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
 import dagger.Module
 import dagger.Provides
 import jakarta.inject.Singleton
 
 @Module
-class AppModule(private val application: Application) {
+class AppModule(private val application: Application , private val preferencesDataStore: DataStore<Preferences>) {
 
     @Provides
     @Singleton
     fun provideContext(): Context = application.applicationContext
+
+    @Singleton
+    @Provides
+    fun providePreferencesDataStore(): DataStore<Preferences> {
+        return preferencesDataStore
+    }
 }
--- app/src/main/java/com/quriogamethechance/quriogame/ui/QurioApp.kt
+++ app/src/main/java/com/quriogamethechance/quriogame/ui/QurioApp.kt
@@ -1,6 +1,8 @@
 package com.quriogamethechance.quriogame.ui
 
 import android.app.Application
+import android.content.Context
+import androidx.datastore.preferences.preferencesDataStore
 import com.quriogamethechance.quriogame.di.AppModule
 
 class QurioApp : Application() {
@@ -9,10 +11,13 @@ class QurioApp : Application() {
     lateinit var appComponent: AppComponent
         private set
 
+    private val preferencesName = "APP_PREFERENCES"
+    val Context.preferencesDataStore by preferencesDataStore("APP_PREFERENCES")
+
     override fun onCreate() {
         super.onCreate()
         appComponent =DaggerAppComponent.builder()
-            .appModule(AppModule(this))
+            .appModule(AppModule(this,preferencesDataStore))
             .build()
     }
 }
